1e77ab83b248e3b056f55678cb3fd751e780c161
NoIgnore <1209172438@qq.com>
2022-08-01 23:49:56 +0800

    完成自动选择坐标

--- Auto.py
+++ Auto.py
@@ -1,15 +1,20 @@
-import win32gui, win32con
+import win32gui
+import win32con
 import win32clipboard as w
 import time
 import re
 from win32gui import *
 import pyautogui
+import os
+import pyautogui as pag
 global widthExit, heightExit, h2, w2
 global index
 index = 1
 NoneList = []
 
-#将消息写入剪贴板
+# 将消息写入剪贴板
+
+
 def setText(text):
     w.OpenClipboard()
     w.EmptyClipboard()
@@ -41,26 +46,26 @@ def GetTitle(name):
         NoneList.append(name)
 
 
-#qq搜索栏搜索指定好友
+# qq搜索栏搜索指定好友
 def searchUser(name, winName):
     global widthExit, heightExit, h2, w2
-    #鼠标定位qq搜索栏
+    # 鼠标定位qq搜索栏
     hand = win32gui.FindWindow('TXGuiFoundation', winName)
     setText(name)
     ClickExit(widthExit, heightExit)
     time.sleep(1)
     win32gui.SendMessage(hand, 770, 0, 0)
-    #表示停止1.5秒再运行（运行太快qq会反应不过来）
+    # 表示停止1.5秒再运行（运行太快qq会反应不过来）
     time.sleep(2)
     win32gui.SendMessage(hand, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
     return hand
 
 
 def sendMessage(n, t, name, msg):
-    #自动定位聊天窗口
+    # 自动定位聊天窗口
     hand = win32gui.FindWindow('TXGuiFoundation', GetTitle(name))
     setText(msg)
-    #重复发送消息
+    # 重复发送消息
     for i in range(1, n + 1):
         ClickExit(w2, h2)
         time.sleep(0.5)
@@ -72,14 +77,14 @@ def sendMessage(n, t, name, msg):
     win32gui.PostMessage(hand, win32con.WM_CLOSE, 0, 0)
 
 
-#按重复次数发送消息
+# 按重复次数发送消息
 def formal(name, msg, winName='QQ'):
     global index
-    n = 1  #次数
-    t = 0  #时间间隔
+    n = 1  # 次数
+    t = 0  # 时间间隔
     searchUser(name, winName)
     time.sleep(1)
-    print("开始发送第", index, "个")
+    print("发送第", index, "个")
     index += 1
     sendMessage(n, t, name, msg)
 
@@ -89,15 +94,40 @@ def ClickExit(x, y):
     pyautogui.click()
 
 
+def GetXY(str):
+    tempX = 0
+    tempY = 0
+    while True:
+        # 获取屏幕的尺寸
+        print("请选择" + str + "的坐标")
+        screenWidth, screenHeight = pag.size()
+        x, y = pag.position()
+        # 返回鼠标的坐标
+        print('屏幕尺寸: (%s %s),  鼠标坐标 : (%s, %s)' %
+              (screenWidth, screenHeight, x, y))
+        # 每个1s中打印一次 , 并执行清屏
+        if tempX == float(x) and tempY == float(y):
+            break
+        else:
+            tempX = float(x)
+            tempY = float(y)
+        time.sleep(2)
+        # 执行系统清屏指令
+        os.system('cls')
+    return tempX, tempY
+
+
 if __name__ == "__main__":
     print("在程序运行前，先打开群聊天窗口并移除遮挡窗口")
-    widthExit = float(input("清空群聊搜索的输入框（像素点）的横坐标："))
-    heightExit = float(input("清空群聊搜索的输入框（像素点）的纵坐标："))
-    w2 = float(input("单独私聊的输入框中心（像素点）的横坐标："))
-    h2 = float(input("单独私聊的输入框中心（像素点）的纵坐标："))
-    stringFileName =input("请输入保存名字的txt文件名(不包括后缀.txt)：")
+    widthExit, heightExit = GetXY("清空按键")
+    w2, h2 = GetXY("输入框中心")
+    print("清空群聊搜索的输入框（像素点）的横坐标为：", widthExit)
+    print("清空群聊搜索的输入框（像素点）的纵坐标为：", heightExit)
+    print("单独私聊的输入框中心（像素点）的横坐标为：", w2)
+    print("单独私聊的输入框中心（像素点）的纵坐标为：", h2)
+    stringFileName = input("请输入保存名字的txt文件名(不包括后缀.txt)：")
     groupL = input("请输入群名：")
-    msg=input("请输入要发的消息内容：")
+    msg = input("请输入要发的消息内容：")
     file_obj = open(".\\" + stringFileName + ".txt", encoding="utf-8")
     all_lines = file_obj.readlines()
     abc = []
@@ -105,6 +135,7 @@ if __name__ == "__main__":
         abc.append(line.rstrip())
     for i in range(len(abc)):
         formal(abc[i], msg, winName=groupL)
-    with open(r'.\\非群里人员.txt', 'w') as f:
-        for i in NoneList:
-            f.write(NoneList[i].rstrip() + '\n')
+    if len(NoneList) > 0:
+        with open(r'.\\非群里人员.txt', 'w') as f:
+            for i in NoneList:
+                f.write(NoneList[i].rstrip() + '\n')
