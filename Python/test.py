3569669c6923b12fcbf41d1d4a8286561c98ce70
NoIgnore <1209172438@qq.com>
2022-08-04 00:39:48 +0800

    Python输入次数按右键

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,11 @@
-import pyautogui
-width = 1280
-height= 611
-pyautogui.moveTo(width, height, duration=0.5)
-pyautogui.click()
-print("test6")
+import time
+from pynput.keyboard import Controller, Key
+
+key = Controller()
+times = int(input("请输入次数："))
+print("请等待3s...")
+time.sleep(3)
+for i in range(times):
+    time.sleep(1)
+    key.press(Key.right)
+    key.release(Key.right)
