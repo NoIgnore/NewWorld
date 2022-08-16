47d82eca3fe788228005a6b94261bd6af9a2aaea
NoIgnore <1209172438@qq.com>
2022-08-16 19:38:55 +0800

    应该没bug了吧

--- Python/群发助手/Auto.py
+++ Python/群发助手/Auto.py
@@ -7,12 +7,10 @@ from win32gui import *
 import pyautogui
 import os
 import pyautogui as pag
-global widthExit, heightExit
-global index
-index = 1
+global widthExit, heightExit, NoneList, index
+index = 0
 NoneList = []
-list2 = []
-#明天要改进：直接foo里面获取到相同文本的句柄就return！
+
 # 将消息写入剪贴板
 
 
@@ -24,7 +22,6 @@ def setText(text):
 
 
 def GetTitle(name):
-    global list2
     hasPerson = False
     tempName = ""
     titles = set()
@@ -32,14 +29,13 @@ def GetTitle(name):
     def foo(hwnd, mouse):
         if IsWindow(hwnd) and IsWindowEnabled(hwnd) and IsWindowVisible(hwnd):
             titles.add(GetWindowText(hwnd))
-            list2.append(hwnd)
 
     EnumWindows(foo, 0)
     lt = [t for t in titles if t]
     lt.sort()
     pattern = re.compile(r'(.*)' + name + r'(.*)')
     for t in lt:
-        if (pattern.match(t)):
+        if (len(t) >= len(name) and pattern.match(t)):
             hasPerson = True
             tempName = t
             break
@@ -47,29 +43,33 @@ def GetTitle(name):
         return tempName
     else:
         NoneList.append(name)
-        return "NothisPeople"
+        return "NothisPeoplehhj_hhj"
 
 
 # qq搜索栏搜索指定好友
+
+
 def searchUser(name, winName):
     # 鼠标定位qq搜索栏
     hand = win32gui.FindWindow('TXGuiFoundation', winName)
     setText(name)
-    ClickPoint(widthExit, heightExit)  #清空搜索栏
+    ClickPoint(widthExit, heightExit)  # 清空搜索栏
     time.sleep(0.5)
-    win32gui.SendMessage(hand, win32con.WM_PASTE, 0, 0)  #粘贴
+    win32gui.SendMessage(hand, win32con.WM_PASTE, 0, 0)  # 粘贴
     # 表示停止1.5秒再运行（运行太快qq会反应不过来）
     time.sleep(1.5)
     win32gui.SendMessage(hand, win32con.WM_KEYDOWN, win32con.VK_RETURN,
-                         0)  #按下回车
-    win32gui.SendMessage(hand, win32con.WM_KEYUP, win32con.VK_RETURN, 0)  #释放回车
+                         0)  # 按下回车
+    win32gui.SendMessage(hand, win32con.WM_KEYUP, win32con.VK_RETURN,
+                         0)  # 释放回车
     return hand
 
 
 def sendMessage(n, t, name, msg):
     # 自动定位聊天窗口
     name2 = GetTitle(name)
-    if name2 == "NothisPeople":
+    if name2 == "NothisPeoplehhj_hhj":
+        print("查无此人：" + name)
         return
     hand = win32gui.FindWindow('TXGuiFoundation', name2)
     setText(msg)
@@ -78,12 +78,12 @@ def sendMessage(n, t, name, msg):
         time.sleep(0.5)
         win32gui.SendMessage(hand, win32con.WM_PASTE, 0, 0)
         win32gui.SendMessage(hand, win32con.WM_KEYDOWN, win32con.VK_RETURN,
-                             0)  #按下回车
+                             0)  # 按下回车
         win32gui.SendMessage(hand, win32con.WM_KEYUP, win32con.VK_RETURN,
-                             0)  #释放回车
+                             0)  # 释放回车
         i = i + 1
         time.sleep(t)
-    time.sleep(3)
+    time.sleep(2)
     win32gui.PostMessage(hand, win32con.WM_CLOSE, 0, 0)
 
 
@@ -95,8 +95,8 @@ def formal(name, msg, winName='QQ'):
     searchUser(name, winName)
     time.sleep(1)
     index += 1
+    print("正在发送第", index, "个......")
     sendMessage(n, t, name, msg)
-    print("发送第", index, "个")
 
 
 def ClickPoint(x, y):
@@ -133,15 +133,24 @@ if __name__ == "__main__":
     print("清空群聊搜索的输入框（像素点）的横坐标为：", widthExit)
     print("清空群聊搜索的输入框（像素点）的纵坐标为：", heightExit)
     stringFileName = input("请输入保存名字的txt文件名(不包括后缀.txt)：")
+    gap = int(input("输入大约间隔时间/s："))
     groupL = input("请输入群名：")
     msg = input("请输入要发的消息内容：")
-    file_obj = open(".\\" + stringFileName + ".txt", encoding="utf-8")
+    try:
+        file_obj = open(".\\" + stringFileName + ".txt", encoding="utf-8")
+        print("UTF-8格式打开文件")
+    except:
+        file_obj = open(".\\" + stringFileName + ".txt",
+                        encoding='gbk',
+                        errors='ignore')
+        print("GBK格式打开文件")
     all_lines = file_obj.readlines()
     abc = []
     for line in all_lines:
         abc.append(line.rstrip())
     for i in range(len(abc)):
         formal(abc[i], msg, winName=groupL)
+        time.sleep(gap)
     if len(NoneList) > 0:
         with open(r'.\\非群里人员.txt', 'w') as f:
             for i in NoneList:
